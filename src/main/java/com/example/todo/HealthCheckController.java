9ac0510590be58f91b787634e42c37af7d8c7dac
ds02168 <1995musso@gmail.com>
2023-01-31 16:53:07 +0900

    # 헬스 수정

--- src/main/java/com/example/todo/HealthCheckController.java
+++ src/main/java/com/example/todo/HealthCheckController.java
@@ -3,11 +3,14 @@ package com.example.todo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @Slf4j
+@CrossOrigin(origins="*")
 public class HealthCheckController {
 
     @GetMapping("/")
